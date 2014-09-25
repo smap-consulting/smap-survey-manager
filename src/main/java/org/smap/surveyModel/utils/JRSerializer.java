0342420cf19e62ef785de50bd02e1f1b86bc16c6
Scott Wells <swellsdev@gmail.com>
2014-09-25 19:49:59 +1000

    still serializing

--- src/main/java/org/smap/surveyModel/utils/JRSerializer.java
+++ src/main/java/org/smap/surveyModel/utils/JRSerializer.java
@@ -1,13 +1,36 @@
 package org.smap.surveyModel.utils;
 
-public class JRSerializer {
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.charset.Charset;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import org.javarosa.core.model.FormIndex;
+import org.javarosa.core.model.instance.TreeReference;
 
+public class JRSerializer implements Serializable{
+	
+	private String surveyDefXml;
+	private String surveyInstanceXml;
+	private String serializedTreeRef;
+	private int localIndex;
+	
+	public JRSerializer(String surveyDefXml, String surveyInstanceXml, FormIndex index){
+		this.surveyDefXml=surveyDefXml;
+		this.surveyInstanceXml=surveyInstanceXml;
+		this.localIndex = index.getLocalIndex();
+		this.serializedTreeRef = serializeTreeRef(index.getReference());
 	}
 
+	public static String serializeTreeRef(TreeReference treeRef){
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        try {
+			treeRef.writeExternal(dos);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new String(baos.toByteArray(), Charset.defaultCharset());
+	}
 }
