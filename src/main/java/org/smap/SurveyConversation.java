18e10aa94a392d2d41f51c906d15d531c2f99201
Scott Wells <swellsdev@gmail.com>
2014-09-28 20:25:14 +1000

    added getAnswers to surveyConversation interface

--- src/main/java/org/smap/SurveyConversation.java
+++ src/main/java/org/smap/SurveyConversation.java
@@ -3,14 +3,16 @@ package org.smap;
 
 
 public interface SurveyConversation {
-	
+
 	public void answerCurrentQuestion(String answerText);
 
 	public Object save();
-	
+
 	public String getCurrentQuestion();
-	
+
 	public String[] getAllQuestions();
-	
+
 	public Boolean isComplete();
+
+    public String getAnswers();
 }
